398a6dd11500022779c75e94c4273529a4d3adca
ejj28 <eriksinventions11@gmail.com>
2020-02-26 21:07:26 -0500

    Renamed main.py to mididrumhero.py

--- mididrumhero.py
+++ mididrumhero.py
@@ -5,38 +5,46 @@ import pyvjoy
 import pygame.midi
 import time
 
+print "\nMidiDrumHero by ejj28"
+print "https://github.com/ejj28/mididrumhero\n"
+
+
 def millis():
 	return int(round(time.time() * 1000))
 
-midiNumbers = []
 
 
-
-
-vController = pyvjoy.VJoyDevice(1)
-
-pygame.midi.init()
-
-count = 0
-for i in range( pygame.midi.get_count() ):
-	r = pygame.midi.get_device_info(i)
-	if r[2]:
-		print str(count) + ": " + str(r[1])
-		midiNumbers.append(i)
-		count += 1
-print "Please enter the ID of the MIDI device to use:"
-userMidiSelection = int(raw_input())
-
-i = pygame.midi.Input(midiNumbers[userMidiSelection])
-
-lastHitTime = [0,0,0,0,0]
-states = [False, False, False, False, False]
-notes = [48,38,43,45,36]
-controllerMappings = [1,2,3,4,5]
-isMapped = [False,False,False,False,False]
-drumNames = ["red drum pad", "yellow drum pad", "blue drum pad", "green drum pad", "kick pedal"]
 
 try:
+
+	vController = pyvjoy.VJoyDevice(1)
+
+	pygame.midi.init()
+
+	midiNumbers = []
+
+	print "Detected MIDI input devices:"
+
+	count = 0
+	for i in range( pygame.midi.get_count() ):
+		r = pygame.midi.get_device_info(i)
+		if r[2]:
+			print str(count) + ": " + str(r[1])
+			midiNumbers.append(i)
+			count += 1
+
+	print "Please enter the ID of the MIDI device to use:"
+	userMidiSelection = int(raw_input())
+
+	i = pygame.midi.Input(midiNumbers[userMidiSelection])
+
+	lastHitTime = [0,0,0,0,0]
+	states = [False, False, False, False, False]
+	notes = [48,38,43,45,36]
+	controllerMappings = [1,2,3,4,5]
+	isMapped = [False,False,False,False,False]
+	drumNames = ["red drum pad", "yellow drum pad", "blue drum pad", "green drum pad", "kick pedal"]
+
 	for t in range(len(controllerMappings)):
 		print "Please hit the " + drumNames[t]
 		while isMapped[t] == False:
@@ -48,7 +56,7 @@ try:
 						isMapped[t] = True
 						print "Mapped " + drumNames[t] + " to Midi note " + str(x[0][1])
 						break
-	print "MidiDrumHero is now translating your E-Kit to Clone Hero, press Ctrl-C to quit"
+	print "MidiDrumHero is now translating your E-Kit to Clone Hero"
 
 
 	while True:
