632b0b44169ef2e5f8e4d8f9ba6cfc7ee10b56c1
ejj28 <eriksinventions11@gmail.com>
2020-02-26 20:45:17 -0500

    Initial code

---
+++ main.py
@@ -0,0 +1,75 @@
+# MidiDrumHero
+# https://github.com/ejj28
+
+import pyvjoy
+import pygame.midi
+import time
+
+def millis():
+	return int(round(time.time() * 1000))
+
+midiNumbers = []
+
+
+
+
+vController = pyvjoy.VJoyDevice(1)
+
+pygame.midi.init()
+
+count = 0
+for i in range( pygame.midi.get_count() ):
+	r = pygame.midi.get_device_info(i)
+	if r[2]:
+		print str(count) + ": " + str(r[1])
+		midiNumbers.append(i)
+		count += 1
+print "Please enter the ID of the MIDI device to use:"
+userMidiSelection = int(raw_input())
+
+i = pygame.midi.Input(midiNumbers[userMidiSelection])
+
+lastHitTime = [0,0,0,0,0]
+states = [False, False, False, False, False]
+notes = [48,38,43,45,36]
+controllerMappings = [1,2,3,4,5]
+isMapped = [False,False,False,False,False]
+drumNames = ["red drum pad", "yellow drum pad", "blue drum pad", "green drum pad", "kick pedal"]
+
+try:
+	for t in range(len(controllerMappings)):
+		print "Please hit the " + drumNames[t]
+		while isMapped[t] == False:
+			if i.poll():
+				midi_events = i.read(10)
+				for x in midi_events:
+					if x[0][2] > 10:
+						notes[t] = x[0][1]
+						isMapped[t] = True
+						print "Mapped " + drumNames[t] + " to Midi note " + str(x[0][1])
+						break
+	print "MidiDrumHero is now translating your E-Kit to Clone Hero, press Ctrl-C to quit"
+
+
+	while True:
+		if i.poll():
+			midi_events = i.read(10)
+			for x in midi_events:
+				
+				for b in range(len(notes)):
+					if x[0][1] == notes[b] and x[0][2] > 10 and states[b] == False:
+						#print "ON, " + str(x[0][1])
+						#print x
+						vController.set_button(controllerMappings[b], 1)
+						lastHitTime[b] = millis()
+						states[b] = True
+				
+		for y in range(len(states)):
+			if states[y] == True and millis() >= lastHitTime[y] + 50:
+				vController.set_button(controllerMappings[y], 0)
+				states[y] = False
+
+					
+except KeyboardInterrupt:
+	pygame.midi.quit()
+
