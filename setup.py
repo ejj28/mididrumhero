71e9cfe13c82cb3f75bc20b68342b7c8ea8fb53f
ejj28 <eriksinventions11@gmail.com>
2020-03-02 10:31:41 -0500

    Created cx_freeze setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
+
+executables = [
+    Executable('mididrumhero.py'),
+    Executable('monitor.py'),
+	Executable('configure.py', base="Win32GUI")
+	]
+
+setup(  name = "MidiDrumHero",
+        version = "1.2",
+        description = "A bridge between E-Kits and Clone Hero",
+        options = {"build_exe": build_exe_options},
+        executables = executables)
