cf515578514e187a416fbdd986292607e1b9669a
Collin Town <tntcommando1@gmail.com>
2020-04-03 21:27:53 -0400

    Updated appending to dropdown

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const {ipcRenderer} = require('electron');
 const shell = require('electron').shell;
 const midi = require('midi');
+const input = new midi.Input();
 
 (function ($) {
     $.fn.serializeFormJSON = function () {
@@ -21,8 +22,6 @@ const midi = require('midi');
     };
 })(jQuery);
 
-const input = new midi.Input();
-
 // Initalize
 $(document).ready(() => {
     var data = ipcRenderer.sendSync('getDrumPads');
@@ -34,19 +33,27 @@ $(document).ready(() => {
     var selected = ipcRenderer.sendSync('getMidiDevice');
     $("#dropdownMidi li a").parents(".dropdown").find('.btn').html(selected.deviceName + ' <span class="caret"></span>');
     $("#dropdownMidi li a").parents(".dropdown").find('.btn').val(selected.deviceVal);
+
+    for (var i = 0; i < input.getPortCount(); i++) {
+        $('#dropdownMidi').append(
+            "<li><a href='#' class='dropdown-item' data-value=" + i.toString() + ">" + input.getPortName(i) + "</a></li>"
+        );
+    }
+
+    $("#saveNewDrumPad").click(function() {
+        var data =  $('#newDrumPadModalForm').serializeFormJSON();
+        ipcRenderer.send('saveDrumPad', data);
+        $('#newDrumPadModalForm').trigger('reset');
+        addToDrumPadTable(data.velocity, data.button, data.midi);
+    });
     
-    var ul = document.getElementById("dropdownMidi");
-    var i;
-    for (i = 0; i < input.getPortCount(); i++) {
-        var a = document.createElement("a");
-        var li = document.createElement("li");
-        a.setAttribute('href', "#");
-        a.setAttribute('class', "dropdown-item");
-        a.setAttribute('data-value', i.toString());
-        a.appendChild(document.createTextNode(input.getPortName(i)));
-        li.appendChild(a);
-        ul.appendChild(li);
-    } 
+    $("#dropdownMidi li a").click(function(){
+        $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+        $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+        ipcRenderer.send('saveMidiDevice', [$(this).text(), $(this).data('value')]);
+        console.log("Hello");
+        input.openPort($(this).data('value'));
+    });
 });
 
 function productDelete(button) {
@@ -65,26 +72,10 @@ function addToDrumPadTable(velocity, button, midi) {
     );
 }
 
-$("#saveNewDrumPad").click(function() {
-    var data =  $('#newDrumPadModalForm').serializeFormJSON();
-    ipcRenderer.send('saveDrumPad', data);
-    $('#newDrumPadModalForm').trigger('reset');
-    addToDrumPadTable(data.velocity, data.button, data.midi);
-});
-
-$("#dropdownMidi li a").click(function(){
-    $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
-    $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
-    ipcRenderer.send('saveMidiDevice', [$(this).text(), $(this).data('value')]);
-    input.openPort($(this).data('value'));
-});
-
 // Social Media
 $("#github").click(function() {
     shell.openExternal("https://github.com/ejj28/mididrumhero");
 });
-
-
 
 input.on('message', (deltaTime, message) => {
   // The message is an array of numbers corresponding to the MIDI bytes:
