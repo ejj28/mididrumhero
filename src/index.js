cf7f69539cf985da910ab2eade3b242747997f48
ejj28 <eriksinventions11@gmail.com>
2020-05-19 16:07:08 -0400

    Updated sidebar

--- src/index.js
+++ src/index.js
@@ -2,24 +2,7 @@ const {ipcRenderer} = require('electron');
 const shell = require('electron').shell;
 
 
-(function ($) {
-    $.fn.serializeFormJSON = function () {
 
-        var o = {};
-        var a = this.serializeArray();
-        $.each(a, function () {
-            if (o[this.name]) {
-                if (!o[this.name].push) {
-                    o[this.name] = [o[this.name]];
-                }
-                o[this.name].push(this.value || '');
-            } else {
-                o[this.name] = this.value || '';
-            }
-        });
-        return o;
-    };
-})(jQuery);
 
 // Initialize
 $(document).ready(() => {
@@ -37,11 +20,7 @@ $(document).ready(() => {
 
     //var keysSwitchState = ipcRenderer.sendSync('getKeysSwitchState');
 
-    // Add drumpads to the table for the user to see
-    var data = ipcRenderer.sendSync('getDrumPads');
-    for (drumPad in data) {
-        addToDrumPadTable(data[drumPad].laneName, data[drumPad].midi, data[drumPad].velocity);
-    }
+    
 
     var midiDevices = ipcRenderer.sendSync('getMidiDevices');
     // Remove the message in dropdown if there are Midi Devices available
@@ -73,40 +52,6 @@ $(document).ready(() => {
     });
 
 });
-
-// Check if the save button in the new drumpad modal was clicked
-$("#saveNewDrumPad").click(function() {
-    var data =  $('#newDrumPadModalForm').serializeFormJSON();
-    if (data.button <= 0 || data.button > 128) {
-        alert("vJoy doesn't support buttons less than or equal to 0, and greater than 128!");
-        $('#newDrumPadModalForm').trigger('reset');
-    }
-    else {
-        ipcRenderer.send('saveDrumPad', data);
-        addToDrumPadTable(data.laneName, data.midi, data.velocity);
-        ipcRenderer.send('changedConfig');
-    }
-});
-
-// Remove drumpad from table and from storage
-function removeDrumPad(button) {
-    ipcRenderer.send('removeDrumPad', $(button).parents("tr").index());
-    $(button).parents("tr").remove();
-    ipcRenderer.send('changedConfig');
-}
-
-// Add a drumpad to the UI table
-function addToDrumPadTable(name, midi, velocity) {
-    $('#drumPadTable tbody').append(
-        "<tr>" +
-            "<th scope='row'>" + name + "</th>" +
-            "<td><a onclick='editDrumPadMidi(this)' href='#'>" + "27" + "</button></td>" +
-            "<td><a onclick='editDrumPadVelocity(this)' href='#'>" + velocity + "</button></td>" +
-            "<td><button type='button' class='btn btn-primary btn-sm' onclick='mapDrumPad(this)'>Map</button></td>" +
-            "<td><button type='button' class='btn btn-danger btn-sm' onclick='clearDrumPad(this)'>Clear</button></td>" +
-        "</tr>"
-    );
-}
 
 $('#keysSwitch').click(() => {
     var checkedValue = document.querySelector('#keysSwitch').checked;
